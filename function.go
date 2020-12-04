805a0a8e40fd50ac9203ed1dcc9400f7a3fc07ad
Walidnurudin <walidnurudin47@gmail.com>
2020-12-05 00:03:35 +0700

    initial commit

---
+++ function.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func penjumlahan(angka1 int, angka2 int) int {
+	return angka1 + angka2
+}
+
+// func main langsung jalan
+func main() {
+	fmt.Println(penjumlahan(10, 10))
+}
---
+++ variabel.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+var nama string = "walid nurudin"
+
+func main() {
+	// shorthand variabel hanya bisa didalam function
+	umur := 19
+	fmt.Println("nama saya "+nama+", umur saya", umur)
+}
