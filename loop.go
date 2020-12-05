66f5513e16ee60753dfb22aa40c0fcaaf31ea8de
Walidnurudin <walidnurudin47@gmail.com>
2020-12-05 23:12:37 +0700

    loop

---
+++ loop.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 1
+	// for i := 0; i < 100; i++ {
+	// 	fmt.Println("Baris ke", i)
+	// }
+
+	// 2
+	// number := 0
+	// for number <= 100 {
+	// 	fmt.Println("Baris ke", number)
+	// 	number++
+	// }
+
+	// 3
+	name := "Walid nurudin"
+	for index, huruf := range name {
+		fmt.Println("index ke", index, "huruf = ", string(huruf))
+	}
+}
