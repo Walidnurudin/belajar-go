4562c03392223bcb00b16ce5630ff5edb29fbc84
Walidnurudin <walidnurudin47@gmail.com>
2020-12-06 10:59:56 +0700

    slice

---
+++ slice.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1
+	// var nama []string
+
+	// nama = append(nama, "Walid")
+	// nama = append(nama, "nurudin")
+
+	// 2
+	nama := []string{"walid", "nurudin"}
+
+	fmt.Println(nama)
+}
